8fd31d77f200aaec48fe73c502796f2a063b48f6
Flamewall <abbamahdi9@gmail.com>
2026-08-15 02:33:27 +0300

    I cant find a way to fix the overlay bug 😭

--- js/pages/List.js
+++ js/pages/List.js
@@ -87,8 +87,7 @@ export default {
                         <p class="error" v-for="error of errors">{{ error }}</p>
                     </div>
                     <div class="og">
-                    <h3>Ownership</h3>
-                        <p id="ownership" class="type-label-md">Credits go to <a href="https://tsl.pages.dev/" target="_blank">TheShittyList.</a> we do not own the original project, however some features are added by developers that may not exist in TheShittyList</p>
+                        <p class="type-label-md">Credits go to <a href="https://tsl.pages.dev/" target="_blank">TheShittyList.</a> we do not own the original project, however some features are added by developers that may not exist in TheShittyList</p>
                     </div>
                     <template v-if="editors">
                         <h3>List Editors</h3>
